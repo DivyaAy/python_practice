8014069060bac1eb475d84825d79ec0e8e90db71
DivyaAy <divyabbplayer@gmail.com>
2021-07-26 14:28:41 +0530

    lis of objects

---
+++ DAY6_TASK/student_list.py
@@ -0,0 +1,17 @@
+class Students:
+    def __init__(self,name,rollno,addmissionno):
+        self.myname = name
+        self.myrollno = rollno
+        self.myaddmissionno = addmissionno
+obj = []
+obj.append(Students("divya",9,4554))
+obj.append(Students("div",99,4555))
+print(obj[0].myname)
+print(obj[1].myrollno)
+#print(obj[2].myaddmissionno)
+#print(obj[0].myname)
+#print(obj[0].myname)
+#print(obj[0].myname)
+
+
+
