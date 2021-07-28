40a7e2fb10602410e493f1feed55fc3453835b95
DivyaAy <divyabbplayer@gmail.com>
2021-07-28 23:14:57 +0530

    Day8

--- DAY7_TASK/overloading.py
+++ DAY7_TASK/overloading.py
@@ -1,9 +1,9 @@
-def add(a,b):
-    return a+b
+def add(a,b,c=0):
+    return a+b+c
     
 def add(a,b,c):
     return a+b+c
 
 
-#print(add(2,4))
+print(add(2,4))
 print(add(4,5,6))
--- __pycache__/DAY8_TASK/listSlice.py
+++
@@ -1,3 +0,0 @@
-List = ["Divya","Amarthiga",9,7,6]
-print(List[1:3])
-print(List[:4])
